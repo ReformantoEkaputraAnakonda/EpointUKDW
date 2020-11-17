a699d0db51ed78c5d5db556fca0b18b07dc53f7e
ReformantoEkaputraAnakonda <74243253+ReformantoEkaputraAnakonda@users.noreply.github.com>
2020-11-17 23:25:40 +0800

    Add files via upload
    GUI Register & Login. belum selesai

---
+++ EpointUKDWtest/src/main/java/com/mycompany/epointukdwtest/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.epointukdwtest;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Main {
+    public static void main(String[] args){
+        Register rg = New Register();
+        rg.setVisible(true);
+        rg.pack();
+        
+    }        
+}
